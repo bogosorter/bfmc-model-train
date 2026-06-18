648d045d326db0671dc1cbf8e3fa978cdccff01f
bogosorter <luiswbarbosa@gmail.com>
2026-06-18 16:31:27 +0100

    updated readme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("runs/detect/train41/weights/last.pt")
+model = YOLO("yolo11s.pt")
 results = model.train(data="datasets/bfmc-generated/data.yaml", epochs=4, imgsz=416)
 
 model.predict(source="test")
